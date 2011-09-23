bd9440ff5408660e36828bfd944b2d032f57e8cf
Rahul Ravu <rahul@Gamestation.localdomain>
2011-09-23 01:48:25 -0400

    Modified Ratings.py. It can now store timestamp information

--- Ratings.py
+++ Ratings.py
@@ -13,30 +13,36 @@ class Ratings:
   def __init__(self,filename,separator='\t'):
     """ A constructer for the ratings class. 
         The argument filename is a file providing the ratings matrix 
-        separator is the character that is used for separating a single entry in the ratings file 
+        separator is used to split the entries of a single rating entry 
     """
     self.data={}
-    """ use a dictionary data structure to store the data """
-    if filename!=None:
-      f=open(filename,'rU')
-      max_user_id=-1
-      max_item_id=-1
-      for line in f:
-        result=line[:-1].split(separator)
-        user_id=int(result[0])
-        item_id=int(result[1])
-        self.data[(user_id,item_id)]=float(result[2])
-        """ The key into the dictionary data structure is a tuple of the form (user_id,item_id) """
-        """ Donno what would be the difference in storing at as a dictionary or using one of the sparse matrix construct from numpy """
-        if user_id>max_user_id:
-          max_user_id=user_id
-        if item_id > max_item_id:
-          max_item_id=item_id
-      print 'Number of ratings provided',len(self.data)
-      print 'Total no of users',max_user_id
-      print 'Total no of items',max_item_id
-      self.max_user_id=max_user_id
-      self.max_item_id=max_item_id
+    """ use a dictionary data structure to store the data
+        the key for this dictionary is of the form (user_id,item_id)
+        the value will be (rating_id,time_stamp)
+        the time stamp value is optional
+    """
+    f=open(filename,'rU')
+
+    max_user_id=-1
+    max_item_id=-1
+
+    for line in f:
+      result=line[:-1].split(separator)
+      user_id=int(result[0])
+      item_id=int(result[1])
+      
+      """ handle cases when timestamp information is provided """
+      if len(result)==3:
+        self.data[(user_id,item_id)]=(float(result[2]),)
+      else:
+        self.data[(user_id,item_id)]=(float(result[2]),int(result[3]))
+      if user_id>max_user_id:
+        max_user_id=user_id
+      if item_id > max_item_id:
+        max_item_id=item_id
+
+    self.max_user_id=max_user_id
+    self.max_item_id=max_item_id
 
   def getRatingsData(self):
     return self.data
@@ -47,8 +53,19 @@ class Ratings:
   def getTotalItems(self):
     return self.max_item_id
 
+  def storeRating(self,user_id,item_id,rating,timestamp=-1):
+    if timestamp == -1:
+      self.data[(user_id,item_id)]=(rating,)
+    else:
+      self.data[(user_id,item_id)]=(rating,timestamp)
+
+    if self.max_user_id<user_id:
+      self.max_user_id=user_id
+    if self.max_item_id<item_id:
+      self.max_item_id=item_id
+
 def main():
-  x=Ratings('/home/rahulravu/python_experiments/Recommendation_Engine/ml-1m/ratings.dat','::');
+  x=Ratings('/home/rahulravu/python_experiments/data/ml-1m/ratings.dat','::');
 
 if __name__=='__main__':
   main()
--- RecommendationEngine.py
+++ RecommendationEngine.py
@@ -44,24 +44,45 @@ class FactorizationBasedEngine(GenericRecommendationEngine):
     data=ratings.getRatingsData()
     regularizer=training_parameters['regularizer']
     update_rate=training_parameters['alpha']
+    split=self.splitData(data,training_parameters['training_split'])
+    training_data=split[0]
+    test_data=split[1]
     for i in range(training_parameters['iterations']):
       print 'At Iteration',(i+1)
-      total_error=0.0
-      for key in data.keys():
+      for key in training_data.keys():
         user_factor=self.users[key[0]].getFactor()
         item_factor=self.items[key[1]].getFactor()
-        true_rating=data[key]
+        true_rating=training_data[key][0]
         prediction=(user_factor*item_factor).sum()
         error=true_rating-prediction
         temp=user_factor+(update_rate*error*item_factor)-(regularizer*user_factor)
         self.users[key[0]].setFactor(temp)
         temp=item_factor+(update_rate*error*user_factor)-(regularizer*item_factor)
         self.items[key[1]].setFactor(temp)
-        total_error+=error*error
-      total_error=math.sqrt(total_error/len(data))
-      print 'The total error is',total_error
+      total_error=0.0
+      for key in test_data.keys():
+        user_factor=self.users[key[0]].getFactor()
+        item_factor=self.items[key[1]].getFactor()
+        prediction=(user_factor*item_factor).sum()
+        total_error+=((test_data[key][0]-prediction)*(test_data[key][0]-prediction))
+      total_error=math.sqrt(total_error/len(test_data))
+      print 'Test Error after iteration',(i+1),' is ',total_error
     print 'Training Complete' 
      
+  def splitData(self,data,training_split):
+    training_data={}
+    test_data={}
+    random_vals=numpy.random.uniform(0.0,1.0,len(data))
+    count=0
+    for key in data.keys():
+      if random_vals[count] > training_split:
+        test_data[key]=data[key]
+      else:
+        training_data[key]=data[key]
+      count+=1
+    print 'Total no of instances in training data',len(training_data)
+    print 'Total no of instances in test data',len(test_data)
+    return (training_data,test_data)
 
   def addNewUser(self,user):
     if self.trained:
@@ -82,10 +103,11 @@ def main():
   ratings=Ratings.Ratings('/home/rahulravu/python_experiments/data/ml-1m/ratings.dat','::')
   engine=FactorizationBasedEngine()
   training_parameters={}
-  training_parameters['iterations']=100
+  training_parameters['iterations']=10
   training_parameters['dimensions']=10
   training_parameters['alpha']=0.01
   training_parameters['regularizer']=0.001
+  training_parameters['training_split']=0.7
   engine.train(ratings,training_parameters)
 
 
